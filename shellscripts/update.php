19a75f1bf59bec1389a19e73b2b6aaeac0e9a394
Reactive Raven <reactiveraven@gmail.com>
2012-07-17 22:57:11 +0100

    adding update of settings folder

--- shellscripts/update.php
+++ shellscripts/update.php
@@ -2,6 +2,10 @@
 
 require "autoloader.php";
 
+chdir(dirname(__FILE__) . "/settings/");
+shell_exec("git pull");
+chdir(dirname(__FILE__));
+
 RRaven\Automation\Server\SettingsFile::manufacture("settings.json")
   ->getServer(gethostname())
   ->update();
